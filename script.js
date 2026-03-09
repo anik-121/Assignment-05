c26be06fbd2f90c0e6babcd70cc409d0b32fde7c
anik-121 <fahimmahmudanik@gmail.com>
2026-03-09 12:09:38 +0600

    js for sign up page created

---
+++ script.js
@@ -0,0 +1,29 @@
+const API = 'https://phi-lab-server.vercel.app/api/v1/lab';
+
+let allIssues = [];
+let activeTab = 'all';
+let searchTimer = null;
+
+
+/* ── LOGIN ─────────────────────────────────── */
+
+document.getElementById('loginBtn').addEventListener('click', login);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Enter' && !document.getElementById('loginPage').classList.contains('hidden')) login();
+});
+
+function login() {
+  const u = document.getElementById('username').value.trim();
+  const p = document.getElementById('password').value.trim();
+  const err = document.getElementById('loginError');
+
+  if (u === 'admin' && p === 'admin123') {
+    err.classList.add('hidden');
+    document.getElementById('loginPage').classList.add('hidden');
+    document.getElementById('dashboardPage').classList.remove('hidden');
+    loadIssues();
+  } else {
+    err.classList.remove('hidden');
+  }
+}
